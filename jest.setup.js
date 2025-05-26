a7e4d92c37f12c4d388b9ef69c96fde0078f2566
Baruk Freitas <baruk.freitas@gmail.com>
2025-05-26 10:15:50 -0300

    bug jest consertado

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,7 @@
 // jest.setup.js
-// Este arquivo é JavaScript puro, sem tipagens TypeScript.
-
 import '@testing-library/jest-dom';
-import React from 'react'; // React é importado para que o JSX dentro dos mocks funcione
+import React from 'react';
 
-// Mock do useRouter do next/navigation
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(() => ({
     push: jest.fn(),
@@ -13,31 +10,24 @@ jest.mock('next/navigation', () => ({
   })),
 }));
 
-// Mock para next/image (componente Image)
 jest.mock('next/image', () => ({
   __esModule: true,
-  default: (props) => {
-    return React.createElement('img', props); // Usando React.createElement para compatibilidade JS
-  },
+  default: (props) => React.createElement('img', props),
 }));
 
-// Mock para useTranslations e useLocale do next-intl
 jest.mock('next-intl', () => ({
   useTranslations: jest.fn((namespace) => (key) => `${namespace}.${key}`),
   useLocale: jest.fn(() => 'en'),
 }));
 
-// Mock para framer-motion
 jest.mock('framer-motion', () => ({
   motion: {
     div: ({ children }) => React.createElement('div', null, children),
     h1: ({ children }) => React.createElement('h1', null, children),
     p: ({ children }) => React.createElement('p', null, children),
-    // Adicione outros elementos conforme usados no seu componente
   },
 }));
 
-// Mock para next/font/google
 jest.mock('next/font/google', () => ({
   Inter: () => ({
     className: 'inter',
@@ -47,32 +37,6 @@ jest.mock('next/font/google', () => ({
   }),
 }));
 
-// Mocks para @mui/material
-jest.mock('@mui/material/Button', () => ({
-  __esModule: true,
-  default: ({ children, ...props }) => {
-    if (props.href) {
-      return React.createElement('a', props, children);
-    }
-    return React.createElement('button', props, children);
-  },
-}));
-
-jest.mock('@mui/material/Typography', () => ({
-  __esModule: true,
-  default: ({ children, component, ...props }) => {
-    const Component = component || 'span';
-    return React.createElement(Component, props, children);
-  },
-}));
-
-// MOCK PARA CardHeader (Causa original do erro 'root')
-jest.mock('@mui/material/CardHeader', () => ({
-  __esModule: true,
-  default: (props) => React.createElement('div', { 'data-testid': 'CardHeader', ...props }, props.children),
-}));
-
-// Mocks para os ícones do MUI
 jest.mock('@mui/icons-material/Facebook', () => ({
   __esModule: true,
   default: (props) => React.createElement('svg', { 'data-testid': 'FacebookIcon', ...props }),
@@ -85,3 +49,27 @@ jest.mock('@mui/icons-material/Instagram', () => ({
   __esModule: true,
   default: (props) => React.createElement('svg', { 'data-testid': 'InstagramIcon', ...props }),
 }));
+
+jest.mock('@mui/material', () => {
+  const originalModule = jest.requireActual('@mui/material');
+
+  return {
+    ...originalModule,
+    Button: ({ children, ...props }) => {
+      if (props.href) {
+        return React.createElement('a', props, children);
+      }
+      return React.createElement('button', props, children);
+    },
+    Typography: ({ children, component, ...props }) => {
+      const Component = component || 'span';
+      return React.createElement(Component, props, children);
+    },
+    CardHeader: (props) => React.createElement('div', { 'data-testid': 'CardHeader', ...props }, props.children),
+    ListItemText: (props) => React.createElement('div', { 'data-testid': 'ListItemText', ...props }, [
+      props.primary && React.createElement('span', { key: 'primary' }, props.primary),
+      props.secondary && React.createElement('span', { key: 'secondary' }, props.secondary),
+      props.children,
+    ]),
+  };
+});
